cd2dc30cb74b90720e07ed75a4d3303ca890d35a
William Bolton <william.ellet@gmail.com>
2017-10-10 18:12:38 -0400

    Config app as Rails API

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.debug_exception_response_format = :api
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
