02ac2096f601171e5748ee68bb97c5e841979047
William Bolton <william.ellet@gmail.com>
2017-10-15 13:01:42 -0400

    Return geo_json from debts controller

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -3,7 +3,12 @@ class DebtsController < ApplicationController
   def index
     @debts = Debt.all.limit(50)
 
-    render json: @debts, status: 200
+    @feature_collection = {}
+    @feature_collection[:type] = "FeatureCollection"
+    @feature_collection[:features] = []
+    @debts.map{ |debt| @feature_collection[:features] << debt.to_geojson }
+
+    render json: @feature_collection.to_json, status: 200
   end
 
 end
