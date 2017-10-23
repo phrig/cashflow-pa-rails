117419266336704d1608a0a7fa8bf08048ca36a1
William Bolton <william.ellet@gmail.com>
2017-10-22 22:02:42 -0400

    Model schema and migration for counties

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022171722) do
+ActiveRecord::Schema.define(version: 20171023014156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,12 @@ ActiveRecord::Schema.define(version: 20171022171722) do
     t.integer "county_fips"
     t.integer "district"
     t.string "county_name"
+  end
+
+  create_table "counties", force: :cascade do |t|
+    t.string "county_name"
+    t.integer "county_fips"
+    t.json "feature"
   end
 
   create_table "debts", force: :cascade do |t|
