e36b2bf952c9a05c5bfa9c37423925df8175f52c
William Bolton <william.ellet@gmail.com>
2017-10-28 14:10:17 -0400

    Method on expenses to return nearby expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,17 @@
 class Expense < ApplicationRecord
+  include RangeFinder
+
+  def self.get_nearby_expenses(lat, long, dist)
+
+    lat_ranges = RangeFinder::find_lat_range(lat, dist)
+    long_ranges = RangeFinder::find_long_range(lat, long, dist)
+
+    expenses = Expense.where('expense_location_1_lat <= ?', lat_ranges[:max_lat])
+                      .where('expense_location_1_lat >= ?', lat_ranges[:min_lat])
+                      .where('expense_location_1_long <= ?', long_ranges[:max_long])
+                      .where('expense_location_1_long >= ?', long_ranges[:min_long])
+    expenses
+
+  end
+
 end
