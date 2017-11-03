859db87ee8f1432c88780d850bfd14b8641afbf6
William Bolton <william.ellet@gmail.com>
2017-11-03 18:41:21 -0400

    My candidates endpoint

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,54 +2,25 @@ class CandidatesController < ApplicationController
   require 'geokit'
   require 'json'
 
-  def get_senate_district(query_point)
+  def get_district_data(query_point) # TODO: Handle nils, errors, etc.
 
-    SenateDistrict.all.map do |district|
-      feature = district.feature
-      points = []
+    candidate_results = []
 
-      feature['geometry']['coordinates'].first.map do |point|
-        points.push(Geokit::LatLng.new(point[1], point[0]))
-      end
+    senate_results = SenateDistrict.get_district_data(query_point)
+    house_results = HouseDistrict.get_district_data(query_point)
+    congressional_results = CongressionalDistrict.get_district_data(query_point)
 
-      polygon = Geokit::Polygon.new(points)
-
-      check_point = Geokit::LatLng.new(query_point[:latitude], query_point[:longitude])
-
-      if polygon.contains?(check_point)
-        @result = district
-      end
-    end
-    return @result
-
+    candidate_results.push(senate_results, house_results, congressional_results)
   end
 
   def show
     query_point = eval(params['point'])
 
-    district = get_senate_district(query_point)
-
-    response_data = { }
-
-    response_data[:candidate_types] = []
-
-    senate_hash = {}
-
-    senate_hash[:type] = 'senate'
-    senate_hash[:candidates] = []
-
-    senate_info = {}
-    senate_info[:s_first_name] = district.s_first_name
-    senate_info[:s_last_name] = district.s_last_name
-    senate_info[:home_county] = district.home_county
-    senate_info[:legislative_district] = district.legislative_district
-    senate_info[:feature] = district.feature
-
-    senate_hash[:candidates].push( senate_info )
-    response_data[:candidate_types].push( senate_hash )
+    results_hash = {}
+    results_hash['candidate_types'] = get_district_data(query_point)
 
     respond_to do |format|
-      format.json { render json: response_data }
+      format.json { render json: results_hash }
     end
 
   end
