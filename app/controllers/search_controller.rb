18bb7a6d681516e44051a7a95d17cf616f121961
William Bolton <william.ellet@gmail.com>
2017-12-09 23:24:17 -0500

    Load PA state data in controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,20 +1,27 @@
 require 'geocoder'
 
 class SearchController < ApplicationController
-	before_action :search_params
+  before_action :search_params
 
-	def get_results
-		@location=Geocoder.search(search_params[:query])
-		if @location
-			render "results"
-		else
-			raise "#{search_params.inspect}Boo"
-		end
-	end
+  def get_results
 
-	private
+    File.open('doc/PA_State.geojson', 'r') do |file|
+      file.each_line do |line|
+        @pa_state_geojson = line
+      end
+    end
 
-	def search_params
-		params.permit(:query)
-	end
+    @location = Geocoder.search(search_params[:query])
+    if @location
+      render "results"
+    else
+      raise "#{search_params.inspect}Boo"
+    end
+  end
+
+  private
+
+  def search_params
+    params.permit(:query)
+  end
 end
