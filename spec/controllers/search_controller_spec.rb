30e439a91a9218c8005da664716340079795e178
William Bolton <william.ellet@gmail.com>
2018-01-14 12:58:03 -0500

    Test error cases

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -24,5 +24,45 @@ RSpec.describe SearchController, type: :controller do
       end
     end
 
+    context "when a result is not in PA" do
+      before { mock_geocoding!({ state: 'Oregon' }) }
+      let(:query) { { query: '97754' } }
+
+      it "is successful" do
+        expect(do_action.response_code).to eq(200)
+      end
+
+      it "loads the PA map data" do
+        do_action
+        expect(assigns(:pa_state_geojson)).to_not be(nil)
+      end
+
+      it 'returns an error with a message' do
+        do_action
+        expect(assigns(:location_error)).to eq( { error: true,
+          message: 'You searched for a location outside Pennsylvania.' } )
+      end
+    end
+
+    context "when a location does not exist" do
+      before { mock_geocoding!({ fake_location: true }) }
+      let(:query) { { query: 'fake_location' } }
+
+      it "is successful" do
+        expect(do_action.response_code).to eq(200)
+      end
+
+      it "loads the PA map data" do
+        do_action
+        expect(assigns(:pa_state_geojson)).to_not be(nil)
+      end
+
+      it 'returns an error with a message' do
+        do_action
+        expect(assigns(:location_error)).to eq( { error: true,
+          message: 'Could not find the location you searched for. Please try again.' } )
+      end
+    end
+  end
 
 end
