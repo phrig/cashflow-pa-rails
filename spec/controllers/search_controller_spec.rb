f41a004359d63a0b4343c43a1d41033ad3f94857
William Bolton <william.ellet@gmail.com>
2018-01-24 18:27:52 -0500

    Clean up style stuff

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -3,17 +3,17 @@ require 'geocoder'
 
 RSpec.describe SearchController, type: :controller do
 
-  describe "#search_results" do
-    let(:query) { {query: "19102"} }
-    subject(:do_action) {get :search_results, params: query }
+  describe '#search_results' do
+    let(:query) { { query: '19102' } }
+    subject(:do_action) { get :search_results, params: query }
 
-    context "when a result is in Pennsylvania" do
+    context 'when a result is in Pennsylvania' do
       before { mock_geocoding! }
-      it "is successful" do
+      it 'is successful' do
         expect(do_action.response_code).to eq(200)
       end
 
-      it "loads the PA map data" do
+      it 'loads the PA map data' do
         do_action
         expect(assigns(:pa_state_geojson)).to_not be(nil)
       end
@@ -24,15 +24,15 @@ RSpec.describe SearchController, type: :controller do
       end
     end
 
-    context "when a result is not in PA" do
+    context 'when a result is not in PA' do
       before { mock_geocoding!({ state: 'Oregon' }) }
       let(:query) { { query: '97754' } }
 
-      it "is successful" do
+      it 'is successful' do
         expect(do_action.response_code).to eq(200)
       end
 
-      it "loads the PA map data" do
+      it 'loads the PA map data' do
         do_action
         expect(assigns(:pa_state_geojson)).to_not be(nil)
       end
@@ -44,15 +44,15 @@ RSpec.describe SearchController, type: :controller do
       end
     end
 
-    context "when a location does not exist" do
+    context 'when a location does not exist' do
       before { mock_geocoding!({ fake_location: true }) }
       let(:query) { { query: 'fake_location' } }
 
-      it "is successful" do
+      it 'is successful' do
         expect(do_action.response_code).to eq(200)
       end
 
-      it "loads the PA map data" do
+      it 'loads the PA map data' do
         do_action
         expect(assigns(:pa_state_geojson)).to_not be(nil)
       end
