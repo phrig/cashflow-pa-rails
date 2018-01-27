6ec555dc2030f8b32218f50e980e313ded530fcb
William Bolton <william.ellet@gmail.com>
2018-01-27 12:28:39 -0500

    Factory and lat_lng test for debt

--- spec/models/debt_spec.rb
+++ spec/models/debt_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Debt, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '.lat_lng' do
+    let(:debt) { FactoryBot.create(:debt) }
+    let(:lat_lng_arr) { [debt.debt_reporting_location_1_lat, debt.debt_reporting_location_1_long] }
+    it 'returns an array with the right lat and long' do
+      expect(debt.lat_lng).to match_array(lat_lng_arr)
+    end
+  end
 end
