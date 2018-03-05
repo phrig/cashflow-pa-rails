556f58f21969007a52297bd46f85ff9111a7fec1
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-03-05 14:18:06 -0500

    change transaction variables to instance variables

--- app/controllers/concerns/transaction_search_concern.rb
+++ app/controllers/concerns/transaction_search_concern.rb
@@ -4,16 +4,16 @@ module TransactionSearchConcern
   def get_transactions(result_lat, result_lng, distance)
     transactions = []
 
-    expenses = Expense.get_nearby(result_lat, result_lng, distance)
-    debts = Debt.get_nearby(result_lat, result_lng, distance)
-    receipts = Receipt.get_nearby(result_lat, result_lng, distance)
-    contributions = Contribution.get_nearby(result_lat, result_lng, distance)
+    @expenses = Expense.get_nearby(result_lat, result_lng, distance)
+    @debts = Debt.get_nearby(result_lat, result_lng, distance)
+    @receipts = Receipt.get_nearby(result_lat, result_lng, distance)
+    @contributions = Contribution.get_nearby(result_lat, result_lng, distance)
 
     transactions
-      .concat(expenses)
-      .concat(debts)
-      .concat(receipts)
-      .concat(contributions)
+      .concat(@expenses)
+      .concat(@debts)
+      .concat(@receipts)
+      .concat(@contributions)
       .sort_by(&:last)
       .map(&:first)
   end
