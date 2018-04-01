04d746f16bbda5222547fe99fe3c71cb08f29407
pca2 <atwaterc@gmail.com>
2018-04-01 15:48:33 -0400

    Add filer not found 404 test

--- spec/controllers/filers_controller_spec.rb
+++ spec/controllers/filers_controller_spec.rb
@@ -4,11 +4,19 @@ require 'geocoder'
 RSpec.describe FilersController, type: :controller do
 
   describe '#show' do
-    let(:filer) { FactoryBot.create(:filer)  }
-    subject(:do_action) { get :show, params: {id: filer.id} }
-    it 'is successful' do
-      do_action
-      expect(response.status).to eq(200)
+    context "when a filer exists" do
+      subject(:do_action) { get :show, params: {id: filer.id} }
+      let(:filer) { FactoryBot.create(:filer)  }
+      it 'is successful' do
+        do_action
+        expect(response.status).to eq(200)
+      end
+    end
+    context "when a filer doesn't exist" do
+      subject(:do_action) { get :show, params: {id: -1} }
+      it 'triggers 404' do
+        expect{do_action}.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
   
