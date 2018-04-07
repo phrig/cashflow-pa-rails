7951e2cf85043ed1ba6866f60d17adf98fb54bc8
William Bolton <william.ellet@gmail.com>
2018-04-07 14:46:50 -0400

    Test for related filers

--- spec/controllers/filers_controller_spec.rb
+++ spec/controllers/filers_controller_spec.rb
@@ -19,5 +19,16 @@ RSpec.describe FilersController, type: :controller do
         expect{ do_action }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
+
+    context 'when a filer has related filers' do
+      let!(:filer) { FactoryBot.create(:filer) }
+      let!(:related_filers) { FactoryBot.create_list(:filer, 3, filer_id: filer.filer_id) }
+      subject(:do_action) { get :show, params: { id: filer.id } }
+      it 'returns the right number of related filers' do
+        do_action
+        expect(assigns(:related_filers).length).to be(4)
+      end
+    end
+
   end
 end
