e74e9c4fdec8a05dfdd3c2b1ebbf2643426fb939
William Bolton <william.ellet@gmail.com>
2018-04-07 13:37:06 -0400

    Regularize spacing and syntax

--- spec/controllers/filers_controller_spec.rb
+++ spec/controllers/filers_controller_spec.rb
@@ -4,21 +4,20 @@ require 'geocoder'
 RSpec.describe FilersController, type: :controller do
 
   describe '#show' do
-    context "when a filer exists" do
-      subject(:do_action) { get :show, params: {id: filer.id} }
-      let(:filer) { FactoryBot.create(:filer)  }
+    context 'when a filer exists' do
+      subject(:do_action) { get :show, params: { id: filer.id } }
+      let(:filer) { FactoryBot.create(:filer) }
       it 'is successful' do
         do_action
         expect(response.status).to eq(200)
       end
     end
+
     context "when a filer doesn't exist" do
-      subject(:do_action) { get :show, params: {id: -1} }
+      subject(:do_action) { get :show, params: { id: -1 } }
       it 'triggers 404' do
-        expect{do_action}.to raise_error(ActiveRecord::RecordNotFound)
+        expect{ do_action }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
-  
-
 end
